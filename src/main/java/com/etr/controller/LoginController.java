00ec1e880e8deeffbc80d29720920bd56d99db63
ypc1996 <624474697@qq.com>
2019-05-21 22:17:02 +0800

    增加配置文件

--- src/main/java/com/etr/controller/LoginController.java
+++ src/main/java/com/etr/controller/LoginController.java
@@ -2,6 +2,7 @@ package com.etr.controller;
 
 import com.etr.util.WxPayUtils;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,13 +16,16 @@ import java.util.Map;
 @RequestMapping("login")
 @RestController
 public class LoginController {
+    @Value("${appid}")
+    private String appid;
+    @Value("${secret}")
+    private String secret;
+    @Value("${grant_type}")
+    private String grant_type;
     //微信小程序登录
     @RequestMapping("/wxLogin")
     public Map<String,String> wxLogin(String code){
-        String appid="";//小程序appid
-        String secret="";//小程序appSecret
         String js_code=code;//登录获取的code
-        String grant_type="";//授权类型，此处只需填写 authorization_code
         String url = "https://api.weixin.qq.com/sns/jscode2session?appid="+appid+"&secret="+secret+"&js_code="+js_code+"&grant_type="+grant_type;
         String responseStr= WxPayUtils.httpRequest(url,"GET",null);
         ObjectMapper mapper = new ObjectMapper();
