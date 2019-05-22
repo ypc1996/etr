17ce22192057b8016068384e018f110517b1b13c
ypc1996 <624474697@qq.com>
2019-05-22 21:22:16 +0800

    pc

--- src/main/java/com/etr/controller/LoginController.java
+++ src/main/java/com/etr/controller/LoginController.java
@@ -4,6 +4,7 @@ import com.etr.util.WxPayUtils;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
@@ -13,7 +14,7 @@ import java.util.Map;
 /**
  * Created by ypc on 2019-5-20.
  */
-@RequestMapping("login")
+@RequestMapping("/login")
 @RestController
 public class LoginController {
     @Value("${appid}")
@@ -23,13 +24,13 @@ public class LoginController {
     @Value("${grant_type}")
     private String grant_type;
     //微信小程序登录
-    @RequestMapping("/wxLogin")
-    public Map<String,String> wxLogin(String code){
+    @RequestMapping(value = "/wxLogin", method = RequestMethod.GET)
+    public Map<String,Object> wxLogin(String code){
         String js_code=code;//登录获取的code
         String url = "https://api.weixin.qq.com/sns/jscode2session?appid="+appid+"&secret="+secret+"&js_code="+js_code+"&grant_type="+grant_type;
         String responseStr= WxPayUtils.httpRequest(url,"GET",null);
         ObjectMapper mapper = new ObjectMapper();
-        Map<String,String> map=null;
+        Map<String,Object> map=null;
         try {
             map = mapper.readValue(responseStr,Map.class);//readValue到一个原始数据类型.
         } catch (IOException e) {
